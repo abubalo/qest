ae36412fafe0bb77a812650e0ceb57564bfb321b
Abubakar Balogun <abzynobalogun@gmail.com>
2024-05-06 20:51:01 +0200

    refactor jest file

--- src/jest.ts
+++ src/jest.ts
@@ -1,14 +1,6 @@
-//qest.ts
 import { glob } from "glob";
 
 const testPattern = "**/*.(spec|test).(ts|js)";
 
-const options = {
-  cwd: "path/to/directory",
-  dot: true,
-  mark: true,
-};
 
-glob(testPattern, (error, files) => {
-  console.log(files);
-});
+glob(testPattern);
